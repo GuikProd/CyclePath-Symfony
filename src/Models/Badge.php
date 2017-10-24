b14aebab7c16c754476a93e3712e951ae7c2a8cc
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-10-24 19:39:36 +0200

    [ADD](Tests + Models)[!CBL] Tests|Started + Models|Started

---
+++ src/Models/Badge.php
@@ -0,0 +1,146 @@
+<?php
+
+/*
+ * This file is part of the CyclePath project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Models;
+
+/**
+ * Class Badge
+ *
+ * @author guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class Badge
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @var \DateTime
+     */
+    private $obtentionDate;
+
+    /**
+     * @var int
+     */
+    private $level;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @var Image
+     */
+    private $image;
+
+    /**
+     * Badge constructor.
+     */
+    public function __construct()
+    {
+        $this->obtentionDate = new \DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId():? int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param string $label
+     */
+    public function setLabel(string $label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return string
+     */
+    public function getObtentionDate(): string
+    {
+        return $this->obtentionDate->format('d-m-Y h:i:s');
+    }
+
+    /**
+     * @return int
+     */
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    /**
+     * @param int $level
+     */
+    public function setLevel(int $level)
+    {
+        $this->level = $level;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser():? User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return Image
+     */
+    public function getImage():? Image
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param Image $image
+     */
+    public function setImage(Image $image)
+    {
+        $this->image = $image;
+    }
+}
---
+++ src/Models/Image.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ * This file is part of the CyclePath project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Models;
+
+/**
+ * Class Image
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class Image
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $alt;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var \DateTime
+     */
+    private $creationDate;
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @var Badge
+     */
+    private $badge;
+
+    /**
+     * Image constructor.
+     */
+    public function __construct()
+    {
+        $this->creationDate = new \DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId():? int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlt(): string
+    {
+        return $this->alt;
+    }
+
+    /**
+     * @param string $alt
+     */
+    public function setAlt(string $alt)
+    {
+        $this->alt = $alt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl(string $url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * This method is ignored because of the __construct() instantiation.
+     *
+     * @codeCoverageIgnore
+     *
+     * @return string
+     */
+    public function getCreationDate(): string
+    {
+        return $this->creationDate->format('d-m-Y h:i:s');
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User $user
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return Badge
+     */
+    public function getBadge():? Badge
+    {
+        return $this->badge;
+    }
+
+    /**
+     * @param Badge $badge
+     */
+    public function setBadge(Badge $badge)
+    {
+        $this->badge = $badge;
+    }
+}
