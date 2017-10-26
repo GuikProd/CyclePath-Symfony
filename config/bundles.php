f466529922ad538cd8543834324f93202e70df7f
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-10-26 09:33:31 +0200

    [ADD](GraphQL)[!CG] GraphQLBundle|Added + Configured

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
 ];
