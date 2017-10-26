e4072a77ee2bd0c3dbc75bbf03ffbf9bddf6730f
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-10-26 09:24:20 +0200

    [ADD](Repositories)[!CBL] Repositories|Added + Tested & Mapping|Updated

---
+++ src/Repository/ImageRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the CyclePath project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class ImageRepository
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class ImageRepository extends EntityRepository
+{
+
+}
