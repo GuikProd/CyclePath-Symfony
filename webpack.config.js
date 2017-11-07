0bcd68bc96103afc05f2bb5da8d73fbeaac5d363
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-11-07 12:04:46 +0100

    [ADD](Sass)[!CF] Sass|Added + Configured

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ Encore
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableSassLoader()
     .enableReactPreset()
     .enableTypeScriptLoader()
 
@@ -16,13 +16,7 @@ Encore
 
     // uncomment to define the assets of the project
     // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .addStyleEntry('css/main', './assets/dev/scss/main.scss')
 ;
 
 module.exports = Encore.getWebpackConfig();
