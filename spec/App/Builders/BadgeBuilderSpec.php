9a33e99982f3fe903ee8c43822e6a96859ae296f
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-12-22 09:16:44 +0100

    tests(spec): progress on spec + CI corrected

---
+++ spec/App/Builders/BadgeBuilderSpec.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the CyclePath project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\App\Builders;
+
+use PhpSpec\ObjectBehavior;
+use App\Models\Interfaces\BadgeInterface;
+use App\Builders\Interfaces\BadgeBuilderInterface;
+
+/**
+ * Class BadgeBuilderSpec
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class BadgeBuilderSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(BadgeBuilderInterface::class);
+    }
+
+    public function should_return_badge_interface()
+    {
+        $this->create()->shouldReturn(BadgeInterface::class);
+    }
+
+    /**
+     * @param BadgeInterface $badge
+     */
+    public function should_accept_badge_interface_and_return_id(BadgeInterface $badge)
+    {
+        $badge->getId()->willReturn(100);
+
+        $this->setBadge($badge)->shouldReturn(BadgeBuilderInterface::class);
+        $this->build()->getId()->shouldReturn(100);
+    }
+}
---
+++ spec/App/Builders/UserBuilderSpec.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the CyclePath project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\App\Builders;
+
+use PhpSpec\ObjectBehavior;
+use App\Models\Interfaces\UserInterface;
+use App\Builders\Interfaces\UserBuilderInterface;
+
+/**
+ * Class UserBuilderSpec
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class UserBuilderSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(UserBuilderInterface::class);
+    }
+
+    public function should_return_user_interface()
+    {
+        $this->create()->shouldReturn(UserInterface::class);
+    }
+
+    /**
+     * @param UserInterface $user
+     */
+    public function should_accept_user_interface_and_return_id(UserInterface $user)
+    {
+        $user->getId()->willReturn(10);
+
+        $this->setUser($user)->shouldReturn(UserBuilderInterface::class);
+        $this->build()->getId()->shouldReturn(10);
+    }
+}
