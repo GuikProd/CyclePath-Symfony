fb8db1762cab2b303e809d3cdb9a98a1258a0268
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2018-01-14 14:19:28 +0100

    feat(homepage): homepage started + styles in progress

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ Encore
     .addEntry('register', './assets/vue/public/Security/register.js')
     .addEntry('user', './assets/vue/public/User/user.js')
     .enableSassLoader()
+    .addStyleEntry('sass_core', './assets/scss/public/core.scss')
+    .addStyleEntry('sass_navbar', './assets/scss/public/navbar.scss')
+    .addStyleEntry('sass_home', './assets/scss/public/home.scss')
+    .addStyleEntry('sass_footer', './assets/scss/public/footer.scss')
 ;
 
 module.exports = Encore.getWebpackConfig();
